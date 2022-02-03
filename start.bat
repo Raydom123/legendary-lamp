a5abc537628ed98f0b024ba010b0446cf60eb612
Raydom123 <85349150+Raydom123@users.noreply.github.com>
2022-02-03 16:57:37 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator @abz2022/add >nul
-net localgroup administrators ABZ /add >nul
+net user administrator @aiko2k6 /add >nul
+net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
@@ -12,5 +12,5 @@ ICACLS C:\Windows\installer /grant administrator:F >nul
 echo  Successful installation !, If RDP is dead, please rebuild again! 
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get an army tunnel, ensure Ngrok_Auth_token is right in settings> Secrets> secret archive. Perhaps your previous VM still runs: https://dashboard.ngrok.com/status/tunnels "
-echo Username: ABZ
-echo Password: @abz2022
+echo Username: administrator
+echo Password: @aiko2k6
